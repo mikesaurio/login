dbe7e3db13100b1995be1500c5654edd1673f107
mikesaurio <mikesaurio@gmail.com>
2014-09-28 11:24:05 -0500

    actualizacion

--- Registro/src/com/curso/registro/RegistroMainActivity.java
+++ Registro/src/com/curso/registro/RegistroMainActivity.java
@@ -8,18 +8,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegistroMainActivity extends Activity {
 
 	private EditText etNombre,etContrasena;
 	private Button btnAceptar;
+	private TextView registro_main_tv_registrate;
 	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_registro_main);
+		
+		registro_main_tv_registrate=(TextView)findViewById(R.id.registro_main_tv_registrate);
+		registro_main_tv_registrate.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(RegistroMainActivity.this,RegistrarseActivity.class);
+
+				startActivity(intent);
+				finish();
+				
+			}
+		});
 		
 		etNombre =(EditText)findViewById(R.id.registro_main_et_nombre);
 		etContrasena=(EditText) findViewById(R.id.registro_main_et_contrasena);
